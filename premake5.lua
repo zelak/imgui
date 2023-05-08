a7d5a30aa3f088618a46cd9d282bf05dea422c9a
Andre Mauricio Zelak <andre.zelak@gmail.com>
2023-05-07 21:32:05 -0300

    Added support to macos universal binaries.
    Signed-off-by: Andre Mauricio Zelak <andre.zelak@gmail.com>

--- premake5.lua
+++ premake5.lua
@@ -30,6 +30,11 @@ project "ImGui"
 		systemversion "latest"
 		cppdialect "C++17"
 
+	filter "system:macosx"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
